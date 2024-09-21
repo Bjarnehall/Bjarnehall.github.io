9d5419f8a3d5cc17ca687bae787b7c859c9e8731
Bjarnehall <Jonas.bjarnehall@outlook.com>
2024-09-21 23:22:01 +0200

    debug js

--- startales3.js
+++ startales3.js
@@ -79,8 +79,8 @@ window.onload = function() {
         }
 
         // // Ensure the ufo stays within the game bounds
-        ufo.x = Phaser.Math.Clamp(ufo.x, ufo.width / 1, config.width - ufo.width / 1);
-        ufo.y = Phaser.Math.Clamp(ufo.y, ufo.height / 1, config.height - ufo.height / 1);
+        ufo.x = Phaser.Math.Clamp(ufo.x, ufo.width / 4, config.width - ufo.width / 4);
+        ufo.y = Phaser.Math.Clamp(ufo.y, ufo.height / 4, config.height - ufo.height / 4);
         //ufo.x.setSize(1500, 80);
 
         // ufo.x = Phaser.Math.Clamp(ufo.x, -1, config.width + 1);
