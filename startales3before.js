07757ae417018d919aa1532533c6ae76c79ff365
Bjarnehall <Jonas.bjarnehall@outlook.com>
2024-09-26 20:29:27 +0200

    upd

--- startales3before.js
+++ startales3before.js
@@ -19,7 +19,7 @@ window.onload = function() {
 
     function preload() {
         // Load assets here
-        this.load.image('sky', 'https://live.staticflickr.com/65535/53995042192_f07b3d47ae_o.png');
+        this.load.image('sky', 'https://bjarnehall.github.io/startalesintrogamemoon.png');
         this.load.image('ufo', 'https://bjarnehall.github.io/UFOUNI2cat.png');
         this.load.image('ufo_up', 'https://bjarnehall.github.io/UFOUNI2upcat.png');
     }
